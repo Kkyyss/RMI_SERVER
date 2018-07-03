6628a6513d121cecc825d677be5966876abc8967
kkyyss <ongkys1994@gmail.com>
2018-07-04 00:56:57 +0800

    update

--- src/com/ky/jacon/server/services/GlobalServiceImpl.java
+++ src/com/ky/jacon/server/services/GlobalServiceImpl.java
@@ -159,17 +159,16 @@ public class GlobalServiceImpl extends UnicastRemoteObject implements GlobalServ
   }
   
   private Book getBookFromRs(ResultSet rs) {
-    Book book = new Book();
     try {
+      Book book = new Book();
       book.setBook_id(rs.getString("book_id"));
       book.setBook_author(rs.getString("book_author"));
       book.setBook_name(rs.getString("book_name"));
       book.setBook_quantity(rs.getInt("book_quantity"));
-      book.setBook_desc(rs.getString("book_desc"));
       book.setBook_isbn(rs.getString("book_isbn"));
       book.setBook_publisher(rs.getString("book_publisher"));
       book.setBook_subject(rs.getString("book_subject"));
-          return book; 
+      return book; 
     } catch (SQLException ex) {
       Logger.getLogger(GlobalServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
       return null;
@@ -177,9 +176,8 @@ public class GlobalServiceImpl extends UnicastRemoteObject implements GlobalServ
   }
   
   private Status getStatusFromRs(ResultSet rs) {
-    Status status = new Status();
-    
     try {
+      Status status = new Status();
       status.setStatus_id(rs.getInt("status_id"));
       status.setStatus_name(rs.getString("status_name"));
       return status;
@@ -273,16 +271,20 @@ public class GlobalServiceImpl extends UnicastRemoteObject implements GlobalServ
 
   @Override
   public List<Issue> getIssues() throws RemoteException {
-    List<Issue> trs = new ArrayList<>();
-    String sql = ""
-            + "SELECT [issue].tr_id, [issue].tr_date, [issue].user_id, [issue].tr_returned_date, "
-            + "[book].*, [status].* FROM [issue]\n"
-            + "INNER JOIN [book] ON [book].book_id = [issue].book_id\n"
-            + "INNER JOIN [status] ON [status].status_id = [issue].status_id";
+
     
     try (Connection conn = DbConn.getInstance().getConnection();
             Statement stmt = conn.createStatement();) {
-      
+      List<Issue> trs = new ArrayList<>();
+      String sql = ""
+              + "SELECT [issue].tr_id, [issue].tr_date, [issue].tr_returned_date, "
+              + "[book].*, [status].*, "
+              + "[student].*, "
+              + "[user].* FROM [issue]\n"
+              + "INNER JOIN [book] ON [book].book_id = [issue].book_id\n"
+              + "INNER JOIN [status] ON [status].status_id = [issue].status_id\n"
+              + "INNER JOIN [student] ON [student].user_id = [issue].user_id\n"
+              + "INNER JOIN [user] ON [user].user_id = [student].user_id";      
       ResultSet rs = stmt.executeQuery(sql);
       
       while (rs.next()) {
@@ -290,6 +292,7 @@ public class GlobalServiceImpl extends UnicastRemoteObject implements GlobalServ
         tr.setTr_id(rs.getString("tr_id"));
         tr.setTr_date(rs.getString("tr_date"));
         String returned_date = rs.getString("tr_returned_date");
+        tr.setTr_student(getStudentFromRs(rs));
         tr.setTr_returned_date(
                 (returned_date == null || returned_date.isEmpty()) ?
                         "---" : returned_date);
@@ -298,6 +301,22 @@ public class GlobalServiceImpl extends UnicastRemoteObject implements GlobalServ
         trs.add(tr);
       }
       return trs;
+    } catch (SQLException ex) {
+      Logger.getLogger(GlobalServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+      return null;
+    }
+  }
+  
+  private Student getStudentFromRs(ResultSet rs) {
+    try {
+        Student student = new Student();
+        student.setEmail(rs.getString("email"));
+        student.setUser_id(rs.getString("user_id"));
+        student.setStudent_id(rs.getString("student_id"));
+        student.setStudent_intake(rs.getString("student_intake"));
+        student.setStudent_name(rs.getString("student_name"));
+        student.setStudent_no(rs.getString("student_no"));
+        return student;
     } catch (SQLException ex) {
       Logger.getLogger(GlobalServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
       return null;
@@ -324,9 +343,9 @@ public class GlobalServiceImpl extends UnicastRemoteObject implements GlobalServ
           sql = "INSERT INTO [book]("
                   + "book_id,book_name,book_author,"
                   + "book_quantity,book_subject,book_publisher,"
-                  + "book_isbn,book_desc)\n"
+                  + "book_isbn)\n"
               + "VALUES\n"
-              + "(?,?,?,?,?,?,?,?)";
+              + "(?,?,?,?,?,?,?)";
           pstmt = conn.prepareStatement(sql);
           pstmt.setString(1, UUID.randomUUID().toString());
           pstmt.setString(2, book.getBook_name());
@@ -335,7 +354,6 @@ public class GlobalServiceImpl extends UnicastRemoteObject implements GlobalServ
           pstmt.setString(5, book.getBook_subject());
           pstmt.setString(6, book.getBook_publisher());
           pstmt.setString(7, book.getBook_isbn());
-          pstmt.setString(8, book.getBook_desc());
 
           int affectedRows = pstmt.executeUpdate();
           conn.commit();
@@ -361,13 +379,28 @@ public class GlobalServiceImpl extends UnicastRemoteObject implements GlobalServ
   public String addIssue(Issue issue) throws RemoteException {
     
     try (Connection conn = DbConn.getInstance().getConnection()) {
-      String sql = "SELECT book_quantity from [book]\n"
-              + "WHERE book_isbn = ?";
+      String sql = "SELECT 0 from [issue] \n"
+              + "WHERE user_id = ? AND book_Id = ? AND status_id = 1";
       
       PreparedStatement pstmt = conn.prepareStatement(sql);
-      pstmt.setString(1, issue.getTr_book().getBook_isbn());
+      
+      pstmt.setString(1, issue.getTr_student().getUser_id());
+      pstmt.setString(2, issue.getTr_book().getBook_id());
       
       ResultSet rs = pstmt.executeQuery();
+      
+      if (rs.next()) {
+        return "This book already issued to the student!";
+      }
+      
+      sql = "SELECT book_quantity from [book]\n"
+              + "WHERE book_isbn = ?";
+      
+      pstmt = conn.prepareStatement(sql);
+      
+      pstmt.setString(1, issue.getTr_book().getBook_isbn());
+      
+      rs = pstmt.executeQuery();
       
       if (rs.next()) {
         if(rs.getInt("book_quantity") <= 0) {
@@ -385,7 +418,7 @@ public class GlobalServiceImpl extends UnicastRemoteObject implements GlobalServ
       try {
         conn.setAutoCommit(false);
         pstmt.setString(1, UUID.randomUUID().toString());
-        pstmt.setString(2, issue.getUser_id());
+        pstmt.setString(2, issue.getTr_student().getUser_id());
         pstmt.setString(3, issue.getTr_book().getBook_id());
 
         int result = pstmt.executeUpdate();
@@ -446,17 +479,20 @@ public class GlobalServiceImpl extends UnicastRemoteObject implements GlobalServ
     try (Connection conn = DbConn.getInstance().getConnection()) {
       String sql = "UPDATE [issue]\n"
               + "SET status_id = 2,\n"
-              + "tr_returned_date = DATETIME('now', 'localtime')\n"
-              + "WHERE tr_id = ?";
+              + "tr_returned_date = DATETIME('now', 'localtime') "
+              + "WHERE user_id = ? AND book_id = ? AND status_id = 1";
 
       PreparedStatement pstmt = conn.prepareStatement(sql);
       
       try {
         conn.setAutoCommit(false);
-        pstmt.setString(1, issue.getTr_id());
+        pstmt.setString(1, issue.getTr_student().getUser_id());
+        pstmt.setString(2, issue.getTr_book().getBook_id());
 
         int rs = pstmt.executeUpdate();
         System.out.println("Number of rows updated: "+ rs);
+        if (rs == 0)
+          return "No such issue/Issue already returned!";
         conn.commit();
       } catch (SQLException ex) {
         Logger.getLogger(GlobalServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
@@ -482,7 +518,7 @@ public class GlobalServiceImpl extends UnicastRemoteObject implements GlobalServ
             + "from [student]\n"
             + "INNER JOIN [user] ON [user].user_id = [student].user_id\n"
             + "WHERE LOWER([student].student_no) = ?";
-    Student student = new Student();
+    
     try (Connection conn = DbConn.getInstance().getConnection();
         PreparedStatement pstmt = conn.prepareStatement(sql)) {
         
@@ -491,20 +527,34 @@ public class GlobalServiceImpl extends UnicastRemoteObject implements GlobalServ
         ResultSet rs = pstmt.executeQuery();
         
         if (rs.next()) {
-          student.setEmail(rs.getString("email"));
-          student.setUser_id(rs.getString("user_id"));
-          student.setStudent_id(rs.getString("student_id"));
-          student.setStudent_intake(rs.getString("student_intake"));
-          student.setStudent_name(rs.getString("student_name"));
-          student.setStudent_no(rs.getString("student_no"));
-          return student;
-        } else {
-          return null;
-        }
-            
+          return getStudentFromRs(rs);
+        }    
     } catch (SQLException ex) {
       Logger.getLogger(GlobalServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
       return null;
     }
+    return null;
+  }
+
+  @Override
+  public Book getBook(String id) throws RemoteException {
+    String sql = "SELECT * from [book]\n"
+            + "WHERE book_isbn = ?";
+    
+    try (Connection conn = DbConn.getInstance().getConnection();
+        PreparedStatement pstmt = conn.prepareStatement(sql)) {
+        
+        pstmt.setString(1, id);
+        
+        ResultSet rs = pstmt.executeQuery();
+        
+        if (rs.next()) {
+          return getBookFromRs(rs);
+        }   
+    } catch (SQLException ex) {
+      Logger.getLogger(GlobalServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+      return null;
+    }   
+    return null;
   }
 }
