9e70380efc74f4ae985329edc2d18a85a3b7350b
kkyyss <ongkys1994@gmail.com>
2018-07-19 23:46:57 +0800

    update

--- src/com/ky/jacon/server/services/GlobalServiceImpl.java
+++ src/com/ky/jacon/server/services/GlobalServiceImpl.java
@@ -8,6 +8,7 @@ package com.ky.jacon.server.services;
 import com.ky.jacon.api.Model.Email;
 import com.ky.jacon.api.Model.Book;
 import com.ky.jacon.api.Model.Issue;
+import com.ky.jacon.api.Model.Role;
 import com.ky.jacon.api.Model.Status;
 import com.ky.jacon.api.Model.Student;
 import com.ky.jacon.api.Model.User;
@@ -39,7 +40,10 @@ public class GlobalServiceImpl extends UnicastRemoteObject implements GlobalServ
   
     @Override
     public User login(User user) throws RemoteException {
-      String sql = "SELECT * FROM [user] WHERE username=? AND password=?";
+      String sql = "SELECT [user].*, [role].role_name"
+              + " FROM [user] \n"
+              + "INNER JOIN [role] ON [role].role_id = [user].role_id \n"
+              + "WHERE username LIKE ? AND password=?";
 
       try (Connection conn = DbConn.getInstance().getConnection();
               PreparedStatement pstmt = conn.prepareStatement(sql)) {
@@ -48,8 +52,7 @@ public class GlobalServiceImpl extends UnicastRemoteObject implements GlobalServ
         
         ResultSet rs = pstmt.executeQuery();
         if (rs.next()) {
-         user.setUser_id(rs.getString("user_id"));
-         return user;
+         return getUserFromRs(rs);
         }
       } catch (SQLException ex) {
         Logger.getLogger(GlobalServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
@@ -60,25 +63,13 @@ public class GlobalServiceImpl extends UnicastRemoteObject implements GlobalServ
 
   @Override
   public User addUser(User user) throws RemoteException {
-    String sql = "SELECT 0 FROM [user] WHERE username=? OR email =?";    
-    
     try (Connection conn = DbConn.getInstance().getConnection()) {
-      PreparedStatement pstmt = conn.prepareStatement(sql);
-      pstmt.setString(1, user.getUsername());
-      pstmt.setString(2, user.getEmail());
-      
-      ResultSet rs = pstmt.executeQuery();
-      
-      if (rs.next()) {
-        return null;
-      }
-      else {
-        try {
           conn.setAutoCommit(false);
-          sql = "INSERT INTO [user](user_id,username,password,email)\n"
+          String sql = "INSERT INTO [user](user_id,username,password,email)\n"
               + "VALUES\n"
               + "(?,?,?,?)";
-          pstmt = conn.prepareStatement(sql);
+          PreparedStatement pstmt = conn.prepareStatement(sql);      
+        try {
           pstmt.setString(1, UUID.randomUUID().toString());
           pstmt.setString(2, user.getUsername());
           pstmt.setString(3, user.getPassword());
@@ -95,8 +86,6 @@ public class GlobalServiceImpl extends UnicastRemoteObject implements GlobalServ
             pstmt.close();
           }
         }
-
-      }
     } catch (SQLException ex) {
       Logger.getLogger(GlobalServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
       return null;
@@ -105,23 +94,55 @@ public class GlobalServiceImpl extends UnicastRemoteObject implements GlobalServ
   }
 
   @Override
-  public void updateUser(User user) throws RemoteException {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-  }
-
-  @Override
-  public void deleteUser(int id) throws RemoteException {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-  }
-
-  @Override
-  public void getUser(String id) throws RemoteException {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+  public String deleteUser(String id) throws RemoteException {
+    String sql = "DELETE FROM [user] WHERE user_id = ?";
+    
+    try (Connection conn = DbConn.getInstance().getConnection()) {
+      conn.setAutoCommit(false);
+      
+      PreparedStatement pstmt = conn.prepareStatement(sql);
+      
+      try {
+        pstmt.setString(1, id);
+        
+        int rs = pstmt.executeUpdate();
+        
+        if (rs <= 0) return "No such user!";
+        
+        conn.commit();
+      } catch (SQLException ex) {
+        Logger.getLogger(GlobalServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+        conn.rollback();
+      } finally {
+        if (pstmt != null)
+          pstmt.close();
+      }
+    
+    } catch (SQLException ex) {
+      Logger.getLogger(GlobalServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+      return ex.toString();
+    }
+    return null;
   }
 
   @Override
   public List<User> getUsers() throws RemoteException {
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    String sql = "SELECT [user].*, [role].role_name from [user] \n"
+            + "INNER JOIN [role] ON [role].role_id = [user].role_id \n"
+            + "WHERE [user].role_id <> 3";
+    try (Connection conn = DbConn.getInstance().getConnection();
+            Statement stmt = conn.createStatement()) {
+
+      ResultSet rs = stmt.executeQuery(sql);
+      List<User> users = new ArrayList<>();
+      while (rs.next()) {
+        users.add(getUserFromRs(rs));
+      }
+      return users;
+  } catch (SQLException ex) {
+      Logger.getLogger(GlobalServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+      return null;
+    }
   }
 
   @Override
@@ -195,7 +216,7 @@ public class GlobalServiceImpl extends UnicastRemoteObject implements GlobalServ
             + "[book].*, [status].*  FROM [issue]\n"
             + "INNER JOIN [book] ON [book].book_id = [issue].book_id\n"
             + "INNER JOIN [status] ON [status].status_id = [issue].status_id\n"
-            + "WHERE [issue].user_id = ?";
+            + "WHERE [issue].user_id = ? AND [issue].status_id != 2";
     
     try (Connection conn = DbConn.getInstance().getConnection();
             PreparedStatement pstmt = conn.prepareStatement(sql);) {
@@ -224,55 +245,8 @@ public class GlobalServiceImpl extends UnicastRemoteObject implements GlobalServ
     }
   }
 
-//  @Override
-//  public int addBatchIssue(List<Issue> issues) throws RemoteException {
-//    String sql = "INSERT INTO [issue](tr_id,tr_date,user_id,book_id)\n"
-//            + "VALUES\n"
-//            + "(?,DATETIME('now', 'localtime'),?,?)";
-//    int count = 0;
-//    int batchSize = issues.size();
-//    int totalRowsAffected = 0;
-//    try (      
-//      Connection conn = DbConn.getInstance().getConnection();
-//      PreparedStatement pstmt = conn.prepareStatement(sql)) {
-//      try {
-//        conn.setAutoCommit(false);
-//        for (int i = 0; i < batchSize; i++) {
-//          Issue issue = issues.get(i);
-//          pstmt.setString(1, UUID.randomUUID().toString());
-//          pstmt.setString(2, issue.getUser_id());
-//          pstmt.setString(3, issue.getTr_book().getBook_id());
-//          pstmt.addBatch();
-//
-//          count++;
-//          if (count % batchSize == 0) {
-//            System.out.println("Commit the batch");
-//            int[] result = pstmt.executeBatch();
-//            System.out.println("Number of rows inserted: "+ result.length);
-//            totalRowsAffected += result.length;
-//            conn.commit();
-//          }
-//        }
-//        return totalRowsAffected;
-//      } catch (SQLException ex) {
-//        Logger.getLogger(GlobalServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
-//        conn.rollback();
-//      } finally {
-//        if (pstmt != null) {
-//          pstmt.close();
-//        }
-//      }
-//    } catch (Exception ex) {
-//      Logger.getLogger(GlobalServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
-//      return -1;
-//    }
-//    return -1;
-//  }
-
   @Override
   public List<Issue> getIssues() throws RemoteException {
-
-    
     try (Connection conn = DbConn.getInstance().getConnection();
             Statement stmt = conn.createStatement();) {
       List<Issue> trs = new ArrayList<>();
@@ -316,6 +290,7 @@ public class GlobalServiceImpl extends UnicastRemoteObject implements GlobalServ
         student.setStudent_intake(rs.getString("student_intake"));
         student.setStudent_name(rs.getString("student_name"));
         student.setStudent_no(rs.getString("student_no"));
+        student.setStudent_ic(rs.getString("student_ic"));
         return student;
     } catch (SQLException ex) {
       Logger.getLogger(GlobalServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
@@ -513,11 +488,11 @@ public class GlobalServiceImpl extends UnicastRemoteObject implements GlobalServ
   @Override
   public Student getStudent(String id) throws RemoteException {
     String sql = "SELECT [student].student_id, "
-            + "[student].student_no, [student].student_name, [student].student_intake, "
+            + "[student].student_no, [student].student_name, [student].student_intake, [student].student_ic, "
             + "[user].* "
             + "from [student]\n"
             + "INNER JOIN [user] ON [user].user_id = [student].user_id\n"
-            + "WHERE LOWER([student].student_no) = ?";
+            + "WHERE [student].student_no LIKE ?";
     
     try (Connection conn = DbConn.getInstance().getConnection();
         PreparedStatement pstmt = conn.prepareStatement(sql)) {
@@ -556,5 +531,252 @@ public class GlobalServiceImpl extends UnicastRemoteObject implements GlobalServ
       return null;
     }   
     return null;
+  }
+
+  @Override
+  public Student addStudent(Student student) throws RemoteException {
+    try (Connection conn = DbConn.getInstance().getConnection()) {
+        conn.setAutoCommit(false);
+        String sql = "INSERT INTO [user](user_id,username,password,role_id,email)\n"
+            + "VALUES\n"
+            + "(?,?,?,3,?)";
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+      try {
+        student.setUser_id(UUID.randomUUID().toString());
+        pstmt.setString(1, student.getUser_id());
+        pstmt.setString(2, student.getStudent_no());
+        pstmt.setString(3, student.getStudent_ic());
+        pstmt.setString(4, student.getEmail());
+
+        int affectedRows = pstmt.executeUpdate();
+
+        if (affectedRows <= 0)
+          return null;
+
+        conn.commit();
+      } catch (SQLException ex) {
+        Logger.getLogger(GlobalServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+        conn.rollback();
+      } finally {
+        if (pstmt != null) {
+          pstmt.close();
+        }
+      }
+    } catch (SQLException ex) {
+      Logger.getLogger(GlobalServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+      return null;
+    }
+    return addStudentProfile(student);  
+  }
+
+  @Override
+  public User getUserByEmail(String email) throws RemoteException {
+    String sql = "SELECT [user].*, [role].role_name from [user] \n"
+            + "INNER JOIN [role] ON [role].role_id = [user].role_id \n"
+            + "WHERE email = ?";
+    
+    try (Connection conn = DbConn.getInstance().getConnection()) {
+      PreparedStatement pstmt = conn.prepareStatement(sql);
+      pstmt.setString(1, email);
+      
+      ResultSet rs = pstmt.executeQuery();
+      
+      if (rs.next()) {
+        return getUserFromRs(rs);
+      }
+    } catch (SQLException ex) {
+      Logger.getLogger(GlobalServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+      return null;
+    }
+    return null;
+  }
+  
+  private User getUserFromRs(ResultSet rs) {
+    try {
+      User user = new User();
+      user.setEmail(rs.getString("email"));
+      user.setPassword(rs.getString("password"));
+      user.setUsername(rs.getString("username"));
+      user.setUser_id(rs.getString("user_id"));
+      Role role = new Role();
+      role.setRole_id(rs.getInt("role_id"));
+      role.setRole_name(rs.getString("role_name"));
+      user.setRole(role);
+      return user;
+    } catch (SQLException ex) {
+        Logger.getLogger(GlobalServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+        return null;
+    }
+  }
+
+  @Override
+  public User getUserByUsername(String username) throws RemoteException {
+    String sql = "SELECT [user].*, [role].role_name from [user] \n"
+            + "INNER JOIN [role] ON [role].role_id = [user].role_id \n"
+            + "WHERE username = ?";
+    
+    try (Connection conn = DbConn.getInstance().getConnection()) {
+      PreparedStatement pstmt = conn.prepareStatement(sql);
+      pstmt.setString(1, username);
+      
+      ResultSet rs = pstmt.executeQuery();
+      
+      if (rs.next()) {
+        return getUserFromRs(rs);
+      }
+    } catch (SQLException ex) {
+      Logger.getLogger(GlobalServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+      return null;
+    }
+    return null;
+  }
+
+  @Override
+  public Student addStudentProfile(Student student) throws RemoteException {
+    try (Connection conn = DbConn.getInstance().getConnection()) {
+        conn.setAutoCommit(false);
+        String sql = "INSERT INTO [student](student_id,user_id,student_no,student_name,student_intake,student_ic)\n"
+            + "VALUES\n"
+            + "(?,?,?,?,?,?)";
+        PreparedStatement pstmt = conn.prepareStatement(sql);
+      try {
+        pstmt.setString(1, UUID.randomUUID().toString());
+        pstmt.setString(2, student.getUser_id());
+        pstmt.setString(3, student.getStudent_no());
+        pstmt.setString(4, student.getStudent_name());
+        pstmt.setString(5, student.getStudent_intake());
+        pstmt.setString(6, student.getStudent_ic());
+
+        int affectedRows = pstmt.executeUpdate();
+
+        if (affectedRows <= 0)
+          return null;
+
+        conn.commit();
+      } catch (SQLException ex) {
+        Logger.getLogger(GlobalServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+        conn.rollback();
+      } finally {
+        if (pstmt != null) {
+          pstmt.close();
+        }
+      }
+    } catch (SQLException ex) {
+      Logger.getLogger(GlobalServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+      return null;
+    }
+    return student;
+  }
+
+  @Override
+  public List<Student> getStudents() throws RemoteException {
+    String sql = "SELECT [student].*, [user].email from [student] \n"
+            + "INNER JOIN [user] ON [user].user_id = [student].user_id";
+    try (Connection conn = DbConn.getInstance().getConnection();
+            Statement stmt = conn.createStatement()) {
+
+      ResultSet rs = stmt.executeQuery(sql);
+      List<Student> students = new ArrayList<>();
+      while (rs.next()) {
+        students.add(getStudentFromRs(rs));
+      }
+      return students;
+  } catch (SQLException ex) {
+      Logger.getLogger(GlobalServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+      return null;
+    }  
+  }
+
+  @Override
+  public String saveUserPwd(User u) throws RemoteException {
+    String sql = "UPDATE [user] \n"
+            + "SET password = ? \n"
+            + "WHERE user_id = ?";
+    
+    try (Connection conn = DbConn.getInstance().getConnection()) {
+      conn.setAutoCommit(false);
+      PreparedStatement pstmt = conn.prepareStatement(sql);
+
+      
+      try {
+      pstmt.setString(1, u.getPassword());
+      pstmt.setString(2, u.getUser_id());
+        int rs = pstmt.executeUpdate();
+        
+        if (rs <= 0) return "No such user!";
+        conn.commit();
+      } catch (SQLException ex) {
+        Logger.getLogger(GlobalServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+        conn.rollback();
+      } finally {
+        if (pstmt != null) {
+          pstmt.close();
+        }
+      }
+    } catch (SQLException ex) {
+      Logger.getLogger(GlobalServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+      return ex.toString();
+    }
+    return null;
+  }
+
+  @Override
+  public String saveUserInfo(User u) throws RemoteException {
+    String sql = "UPDATE [user] \n"
+            + "SET username = ?, \n"
+            + "email = ? \n"
+            + "WHERE user_id = ?";
+    
+    try (Connection conn = DbConn.getInstance().getConnection()) {
+      conn.setAutoCommit(false);
+       PreparedStatement pstmt = conn.prepareStatement(sql);
+       
+      try {
+      pstmt.setString(1, u.getUsername());
+      pstmt.setString(2, u.getEmail());
+      pstmt.setString(3, u.getUser_id());        
+        int rs = pstmt.executeUpdate();
+        
+        if (rs <= 0) return "No such user!";
+        conn.commit();
+      } catch (SQLException ex) {
+        Logger.getLogger(GlobalServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+        conn.rollback();
+      } finally {
+        if (pstmt != null) {
+          pstmt.close();
+        }
+      }
+    } catch (SQLException ex) {
+      Logger.getLogger(GlobalServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+      return ex.toString();
+    }
+    return null;
+  }
+
+  @Override
+  public Student getStudentProfile(String uid) throws RemoteException {
+    String sql = "SELECT [student].student_id, "
+            + "[student].student_no, [student].student_name, [student].student_intake, [student].student_ic, "
+            + "[user].* "
+            + "from [student]\n"
+            + "INNER JOIN [user] ON [user].user_id = [student].user_id\n"
+            + "WHERE [student].user_id = ?";
+    
+    try (Connection conn = DbConn.getInstance().getConnection();
+        PreparedStatement pstmt = conn.prepareStatement(sql)) {
+        
+        pstmt.setString(1, uid);
+        
+        ResultSet rs = pstmt.executeQuery();
+        
+        if (rs.next()) {
+          return getStudentFromRs(rs);
+        }    
+    } catch (SQLException ex) {
+      Logger.getLogger(GlobalServiceImpl.class.getName()).log(Level.SEVERE, null, ex);
+      return null;
+    }
+    return null;  
   }
 }
